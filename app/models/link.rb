1f819bb955634996c7f2b9a0de7189c67afcdf7b
mdromi <mdromi14@gmail.com>
2024-02-08 15:30:32 +0600

    Add view chart

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,16 @@
 class Link < ApplicationRecord
-    scope :recent_first, -> {order(created_at: :desc)}
+    has_many :views, dependent: :destroy
+  
     validates :url, presence: true
-end
+  
+    scope :recent_first, -> { order(created_at: :desc) }
+  
+    def self.find(id)
+      super(ShortCode.decode(id))
+    end
+  
+    def to_param
+      ShortCode.encode(id)
+    end
+  end
+  
