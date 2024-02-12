bf33646e617af9b8a7902f0751bf5d5bb3dcf186
mdromi <mdromi14@gmail.com>
2024-02-12 21:11:02 +0600

    Add view profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,19 @@
 class ProfilesController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-      end
+  before_action :authenticate_user!
+  before_action :set_user_and_links, only: :show
+
+  def show
+    if current_user.admin? || current_user == @user
+      render 'show'
+    else
+      redirect_to root_path, alert: "You are not authorized to view this profile."
+    end
+  end
+
+  private
+
+  def set_user_and_links
+    @user = User.find(params[:id])
+    @pagy, @user_links = pagy(Link.where(user_id: @user.id))
+  end
 end
